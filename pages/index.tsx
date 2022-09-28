2c5b226fd00a299357f50ef9bf9e302149faaa7b
october-03 <october03@o365sen.net>
2022-09-29 01:20:45 +0900

    초안 완료

--- pages/index.tsx
+++ pages/index.tsx
@@ -94,6 +94,12 @@ const Home: NextPage = () => {
           </HelloTextWrapper>
         </ContentWrapper>
         <ContentWrapper>
+          <SubTitle>연락처</SubTitle>
+          <ListWrapper>
+            <List><Link href='mailto:season_autumn@kakao.com' target={'_blank'}>season_autumn@kakao.com</Link></List>
+          </ListWrapper>
+        </ContentWrapper>
+        <ContentWrapper>
           <Title>기술스택</Title>
           <ListWrapper>
             <List>TypeScript</List>
@@ -131,6 +137,40 @@ const Home: NextPage = () => {
             <List>{`React Native 기반의 TiOS PAY -> AP First로의 전반적인 리뉴얼된 화면 개발 작업을 하였습니다.`}</List>
             <List>선물하기 관련 기능, 화면들을 중점적으로 개발했습니다</List>
             <List><Link href='https://play.google.com/store/apps/details?id=com.keepercustomerapp&hl=ko&gl=US' target='_blank'>LINK</Link></List>
+          </ListWrapper>
+          <SubTitle>SOOSIM 17.5 개발</SubTitle>
+          <ListWrapper>
+            <List>개인 프로젝트</List>
+            <List>개발기간: 2022.07</List>
+            <ListWrapper>
+              <List>React</List>
+              <List>TypeScript</List>
+            </ListWrapper>
+            <List>{`한양사이버대학교 디자인학부 졸업전시회 17.5회 수심 사이트 개발을 하였습니다`}</List>
+            <List><Link href='https://portal.hanyangcyber.ac.kr/Popup/GrdExhibition/17_5th/index.html' target='_blank'>LINK</Link></List>
+          </ListWrapper>
+          <SubTitle>롤 전적검색 및 팀 매칭 사이트 개발</SubTitle>
+          <ListWrapper>
+            <List>개인 프로젝트</List>
+            <List>개발기간: 2022.09 ~ 작업중</List>
+            <ListWrapper>
+              <List>React</List>
+              <List>TypeScript</List>
+              <List>Next.js</List>
+              <List>Node.js</List>
+              <List>Express</List>
+              <List>MongoDB</List>
+            </ListWrapper>
+            <List>{`현재 작업중에 있으며 Node.js, Express, MongoDB를 기반으로 한 API는 개발이 완료되었습니다.`}</List>
+          </ListWrapper>
+        </ContentWrapper>
+        <ContentWrapper>
+          <Title>LINK</Title>
+          <ListWrapper>
+            <List><Link href='https://github.com/october-03' target={'_blank'}>GITHUB</Link></List>
+            <List><Link href='https://www.rocketpunch.com/@autumnoctoberbus' target={'_blank'}>ROCKETPUNCH</Link></List>
+            <List><Link href='https://velog.io/@october03' target={'_blank'}>BLOG</Link></List>
+            <List><Link href='https://www.linkedin.com/in/jae-young-kim-761408220/' target={'_blank'}>LINKEDIN</Link></List>
           </ListWrapper>
         </ContentWrapper>
       </InnerWrapper>
